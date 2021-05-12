aee2de8851cdc42c1e56c4ceec62e587d2c9e45d
Chhour Thaisan <sanezzy17@gmail.com>
2021-05-12 22:13:49 +0700

    update env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    url: "mongodb+srv://chhean:1234@cluster0.eqwzy.mongodb.net/DevTutor?retryWrites=true&w=majority"
+    url: process.env.DATABASE
   };
